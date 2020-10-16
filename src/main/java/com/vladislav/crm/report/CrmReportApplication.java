4447dfcd8a4b78a69973eb60fc2eb1e339b7f306
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-16 13:30:40 +0300

    init

---
+++ src/main/java/com/vladislav/crm/report/CrmReportApplication.java
@@ -0,0 +1,13 @@
+package com.vladislav.crm.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmReportApplication.class, args);
+    }
+
+}
