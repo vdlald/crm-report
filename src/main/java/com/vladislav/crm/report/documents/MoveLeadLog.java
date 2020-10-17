9c9af74fa27e372c5816b5a84bc5298cece7598f
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-17 11:48:28 +0300

    pojo update

--- src/main/java/com/vladislav/crm/report/documents/MoveLeadLog.java
+++ src/main/java/com/vladislav/crm/report/documents/MoveLeadLog.java
@@ -2,26 +2,32 @@ package com.vladislav.crm.report.documents;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Accessors(chain = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Document(collection = "moveLeadLogs")
 public class MoveLeadLog {
 
     @MongoId
+    @EqualsAndHashCode.Include
     private UUID id = UUID.randomUUID();
 
     private Long userId;
     private Long leadId;
     private Long prevStatusId;
     private Long nextStatusId;
+
+    private LocalDateTime happenedAt = LocalDateTime.now();
 
 }
