ea319012536f51b1a8ef88ce0214c8b36a0071de
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-17 14:02:12 +0300

    refactor

--- src/main/java/com/vladislav/crm/report/operations/impl/GetAllMoveLeadLogByUserIdOperationImpl.java
+++ src/main/java/com/vladislav/crm/report/operations/impl/GetAllMoveLeadLogByUserIdOperationImpl.java
@@ -1,22 +1,26 @@
 package com.vladislav.crm.report.operations.impl;
 
 import com.vladislav.crm.report.documents.MoveLeadLog;
-import com.vladislav.crm.report.operations.ReadAllMoveLeadLogByUserIdOperation;
+import com.vladislav.crm.report.operations.GetAllMoveLeadLogByUserIdOperation;
 import com.vladislav.crm.report.repositories.MoveLeadLogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class ReadAllMoveLeadLogByUserIdOperationImpl implements ReadAllMoveLeadLogByUserIdOperation {
+public class GetAllMoveLeadLogByUserIdOperationImpl
+        implements GetAllMoveLeadLogByUserIdOperation {
 
     private final MoveLeadLogRepository repository;
 
     @Override
-    public Collection<MoveLeadLog> execute(Long userId) {
-        return repository.findAllByUserId(userId);
+    public Collection<MoveLeadLog> execute(
+            Long userId, LocalDateTime happenedAt, LocalDateTime happenedAt2
+    ) {
+        return repository.findAllByUserIdAndHappenedAtIsBetween(userId, happenedAt, happenedAt2);
     }
 }
--- src/main/java/com/vladislav/crm/report/repositories/MoveLeadLogRepository.java
+++ src/main/java/com/vladislav/crm/report/repositories/MoveLeadLogRepository.java
@@ -2,13 +2,18 @@ package com.vladislav.crm.report.repositories;
 
 import com.vladislav.crm.report.documents.MoveLeadLog;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.UUID;
 
 @Repository
 public interface MoveLeadLogRepository extends MongoRepository<MoveLeadLog, UUID> {
 
-    Collection<MoveLeadLog> findAllByUserId(Long userId);
+    @Query(value = "{ 'userId' : ?0, 'happenedAt' : { $gte: ?1, $lte: ?2 } }", sort = "{ 'happenedAt': 1 }")
+    Collection<MoveLeadLog> findAllByUserIdAndHappenedAtIsBetween(
+            Long userId, LocalDateTime happenedAt, LocalDateTime happenedAt2
+    );
 }
