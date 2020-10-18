ff7be2a6591eb3202168a32ee231030d7e6f7b69
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-10-18 11:56:07 +0300

    remove unused code

--- src/main/java/com/vladislav/crm/report/operations/GetAllUserIdFromMoveLeadLogsOperation.java
+++
@@ -1,7 +0,0 @@
-package com.vladislav.crm.report.operations;
-
-import java.util.Set;
-
-public interface GetAllUserIdFromMoveLeadLogsOperation {
-    Set<Long> execute();
-}
--- src/main/java/com/vladislav/crm/report/repositories/MoveLeadLogRepository.java
+++ src/main/java/com/vladislav/crm/report/repositories/MoveLeadLogRepository.java
@@ -12,11 +12,6 @@ import java.util.UUID;
 @Repository
 public interface MoveLeadLogRepository extends MongoRepository<MoveLeadLog, UUID> {
 
-    @Query(value = "{ 'userId' : ?0, 'happenedAt' : { $gte: ?1, $lte: ?2 } }", sort = "{ 'happenedAt': 1 }")
-    Collection<MoveLeadLog> findAllByUserIdAndHappenedAtIsBetween(
-            Long userId, LocalDateTime happenedAt, LocalDateTime happenedAt2
-    );
-
     @Query(value = "{ 'happenedAt' : { $gte: ?0, $lte: ?1 } }", sort = "{ 'happenedAt': 1 }")
     Collection<MoveLeadLog> findAllByHappenedAtBetween(LocalDateTime happenedAt, LocalDateTime happenedAt2);
 }
