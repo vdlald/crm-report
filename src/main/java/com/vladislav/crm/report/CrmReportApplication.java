c94359a2356b0a76d9dc4988bd58f24f2bbbe360
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-11-06 18:03:27 +0300

    remove reduant code

--- src/main/java/com/vladislav/crm/report/CrmReportApplication.java
+++ src/main/java/com/vladislav/crm/report/CrmReportApplication.java
@@ -1,28 +1,15 @@
 package com.vladislav.crm.report;
 
-import com.vladislav.crm.report.repositories.MoveLeadLogRepository;
-import com.vladislav.crm.report.repositories.NewLeadLogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class CrmReportApplication implements ApplicationRunner {
-
-    private final MoveLeadLogRepository moveLeadLogRepository;
-    private final NewLeadLogRepository newLeadLogRepository;
+public class CrmReportApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CrmReportApplication.class, args);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        moveLeadLogRepository.deleteAll();
-        newLeadLogRepository.deleteAll();
     }
 }
